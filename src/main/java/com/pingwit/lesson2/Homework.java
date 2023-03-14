2a4d5d0d8cc3ef422062d1671461363815954119
Mutapeko <misha.kazak99@mail.ru>
2023-03-14 13:58:13 +0300

    Lesson 2

---
+++ src/main/java/com/pingwit/lesson2/Homework.java
@@ -0,0 +1,20 @@
+package com.pingwit.lesson2;
+
+public class Homework {
+    public static void main(String[] args) {
+        float x = 10.1f;
+        float y = 5.5f;
+        double sum = x + y;
+        System.out.println("Sum result  = " + sum);
+
+        double sub = x - y;
+        System.out.println("Sub result = " + sub);
+
+        float div = x / y;
+        System.out.println("Div result = " + div);
+
+        float mul = x * y;
+        System.out.println("Mul result = " + mul);
+    }
+
+}
